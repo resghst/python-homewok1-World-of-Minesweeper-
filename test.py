f541c17bf154ea3148b805011e1fd1394508d0ec
resghst <hank181017@gmail.com>
2018-04-29 04:53:56 +0800

    file update

--- test.py
+++
@@ -1,9 +0,0 @@
-
-# a = [[]*5 for i in range(0,9)]
-a =[[[float(0) for k in range(3)] for j in range(9)] for i in range(9)]
-# for i in range(0,9):
-#     for j in range(0,9):
-#         a[i][j] = [float(0),float(1),float(2)]
-a[0][0][0] =3
-for i in a:
-    print i
